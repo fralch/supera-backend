4fb38896fe8e382b05e23bff8cc2c42db5b6137e
Frank Cairampoma <ingfralch@gmail.com>
2024-05-01 20:01:39 -0500

    Agregar configuración de CORS en main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import {CasosModule} from './casos/casos.module';
 
+
 @Module({
   imports: [UsuarioModule,CasosModule],
   controllers: [],
