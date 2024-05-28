fece2e9ddcbb9258b48517098431ad892db7417d
FrankCairampoma <ingfralch@gmail.com>
2024-05-28 11:48:27 -0500

    feat: Eliminar tabla de migración inicial y actualizar controlador de casos

--- src/casos/casos.controller.ts
+++ src/casos/casos.controller.ts
@@ -6,28 +6,12 @@ import { Casos } from "@prisma/client";
 export class CasosController {
   constructor(private casosService: CasosService) {}
 
-  @Post()
-  async create(@Body() data: Casos): Promise<Casos> {
-    return this.casosService.create(data);
-  }
+ 
 
   @Get()
   async findAll(): Promise<Casos[]> {
     return this.casosService.findAll();
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Casos> {
-    return this.casosService.findOne(Number(id));
-  }
-
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() data: Casos): Promise<Casos> {
-    return this.casosService.update(Number(id), data);
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string): Promise<Casos> {
-    return this.casosService.remove(Number(id));
-  }
+  
 }
