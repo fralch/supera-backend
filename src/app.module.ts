d5f79b3bc6501c6b7047da183024f2233766bc7e
FrankCairampoma <ingfralch@gmail.com>
2024-05-31 12:16:27 -0500

    chore: Refactorizar importaciones en app.module.ts y main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
-import {CasosModule} from './casos/casos.module';
+import { CasosModule } from './casos/casos.module';
 import { PagosModule } from './pagos/pagos.module';
 import { ClientesModule } from './clientes/clientes.module';
 
-
 @Module({
-  imports: [UsuarioModule,CasosModule, PagosModule, ClientesModule],
+  imports: [UsuarioModule, CasosModule, PagosModule, ClientesModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // <-- permite todas las conexiones
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // <-- permite todos los métodos HTTP, incluyendo OPTIONS
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }); // habilitar CORS para todas las rutas
   await app.listen(3000);
 }
 bootstrap();
