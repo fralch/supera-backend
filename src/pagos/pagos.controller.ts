05bdd9a608ee992cde48fc4e752541688997e65d
FrankCairampoma <ingfralch@gmail.com>
2024-06-22 13:15:27 -0500

    feat: Agregar endpoint para crear un nuevo pago

--- src/pagos/pagos.controller.ts
+++ src/pagos/pagos.controller.ts
@@ -1,17 +1,39 @@
-import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { PagosService } from "./pagos.service";
-import { Pagos } from "@prisma/client";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PagosService } from './pagos.service';
+import { Pagos } from '@prisma/client';
 
 @Controller('pagos')
 export class PagosController {
-  constructor(private casosService: PagosService) {}
+  constructor(private pagosService: PagosService) {}
 
-    @Get()
-    async findAll(): Promise<Pagos[]> {
-        return this.casosService.findAll();
+  @Get()
+  async findAll(): Promise<Pagos[]> {
+    return this.pagosService.findAll();
+  }
+
+  @Post()
+  async create(@Body() data) {
+    /* 
+    {
+      "caso_id": 1, 
+      "monto": 125.6, 
+      "fecha_pago": "2024-06-17T22:03:58.000Z",
+      "descripcion": "Descrip", 
+      "metodo_pago": "yape", 
+      "monto_total":  123.00, 
+      "saldo_restante" : 153.00, 
+      "estado": "pendiente"
     }
- 
-
-
-  
+    */
+    return this.pagosService.create(data);
+  }
 }
--- src/pagos/pagos.service.ts
+++ src/pagos/pagos.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Casos,  Pagos, Prisma } from '@prisma/client';
+import { Casos, Pagos, Prisma } from '@prisma/client';
 
 @Injectable()
-
 export class PagosService {
-    constructor(private prisma: PrismaService) {}
-    
-    async findAll(){
-        // buscar todos los pagos y sus casos relacionados
-        return this.prisma.pagos.findMany({
-            include: {
-                // incluir a los clientes relacionados con los casos
-                caso: true,
-                
-            },
-        });
-    }
-   
+  constructor(private prisma: PrismaService) {}
 
+  async findAll() {
+    // buscar todos los pagos y sus casos relacionados
+    return this.prisma.pagos.findMany({
+      include: {
+        // incluir a los clientes relacionados con los casos
+        caso: true,
+      },
+    });
+  }
+
+  async create(data: Prisma.PagosCreateInput) {
+    // crear un nuevo pago
+    return this.prisma.pagos.create({
+      data,
+    });
+  }
 }
